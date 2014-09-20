9f056f3cf3031ec15fa231b608f5da9d05a7caf6
Gabor Csardi <csardi.gabor@gmail.com>
2014-09-20 12:06:42 -0400

    Proper CRANDB update script, first try

--- .openshift/cron/minutely/update-crandb.r
+++ .openshift/cron/minutely/update-crandb.r
@@ -1,5 +1,42 @@
 #! /sandbox/r/bin/Rscript
 
-out_dir <- Sys.getenv("OPENSHIFT_REPO_DIR")
-cat(Sys.time(), "\n", file=file.path(out_dir, "rout.txt"))
+## We need to create a function, because we want to use
+## on.exit.
 
+do <- function() {
+  out_dir <- Sys.getenv("OPENSHIFT_REPO_DIR")
+  pid_file <- file.path(out_dir, "update-crandb.pid")
+
+  ## If the pid file exists, then we only try in every five
+  ## minutes. In case the previous process got stuck
+  minute <- as.numeric(format(Sys.time(), "%M"))
+  if (file.exists(pid_file) && minute %% 5) { return() }
+
+  ## OK, here we go, write pid file
+  pid <- Sys.getpid()
+  cat(pid, "\n", file = pid_file)
+
+  ## Remove pid file, but only if it was written by us.
+  ## Yes, this is a race condition, but it not critical if
+  ## we remove someone else's pid
+  clean_pid <- function() {
+    if (scan(pid_file)[1] == pid) { try(unlink(pid_file)), silent = TRUE }
+  }
+  on.exit(clean_pid(), add = TRUE)
+
+  ## Load packages
+  library(crandb)
+
+  ## Set up config
+  crandb:::couchdb_server("https://db.r-pkg.org/")
+  crandb:::couchdb_server("https://db.r-pkg.org:6984/cran", root = TRUE)
+  options(repos = structure(c(CRAN = "http://cran.r-project.org")))
+
+  ## Do the update, and log it
+  log_file <- file.path("OPENSHIFT_DATA_DIR", "update-crandb.log")
+  cat(format(Sys.time()), " updating... ", file = log_file)
+  crandb:::crandb_update()
+  cat("DONE\n", file = log_file)
+}
+
+do()
