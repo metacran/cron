6f9933ccd2632fdb1af5e9568fd19916dc50776a
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-18 15:45:11 -0400

    Update all task views once a day

---
+++ .openshift/cron/daily/update-task-views.r
@@ -0,0 +1,18 @@
+#! /sandbox/r/bin/Rscript
+
+## It is better to create a function, so that you can use on.exit.
+
+do <- function() {
+
+  URL <- "http://cran.rstudio.com/src/contrib/Views.rds"
+  con <- gzcon(url(URL))
+  on.exit(close(con))
+
+  views <- readRDS(con)
+  names <- vapply(v, "[[", "", "name")
+  for (v in views) {
+    httr::GET(paste0("http://docs.r-pkg.org/api/build/task-view/", v$name))
+  }
+}
+
+do()
