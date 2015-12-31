27a15e8403c8898574eb808af0c2414868a5bf56
Gabor Csardi <csardi.gabor@gmail.com>
2015-12-31 14:05:38 +0000

    Call cranatgh to update the GitHub mirror

--- .openshift/cron/minutely/update-crandb.r
+++ .openshift/cron/minutely/update-crandb.r
@@ -41,7 +41,24 @@ do <- function() {
   data_dir <- Sys.getenv("OPENSHIFT_DATA_DIR")
   log_file <- file.path(data_dir, "update-crandb.log")
   cat(format(Sys.time()), " updating... ", file = log_file, append = TRUE)
-  crandb:::crandb_update()
+  events <- crandb:::crandb_update()
+
+  cat("new: ", file = log_file, append = TRUE)
+  for (p in events$new) {
+    Sys.sleep(1)
+    httr::GET(paste0("http://cranatgh.jenkins.r-pkg.org/add/", p))
+    cat(p, "", file = log_file, append = TRUE)
+  }
+
+  cat("updated: ", file = log_file, append = TRUE)
+  for (p in events$updated) {
+    Sys.sleep(1)
+    httr::GET(paste0("http://cranatgh.jenkins.r-pkg.org/add/", p))
+    cat(p, "", file = log_file, append = TRUE)
+  }
+
+  cat("archived: ", paste(collapse = " ", events$archived),
+      file = log_file, append = TRUE)
   cat("DONE\n", file = log_file, append = TRUE)
 
   ## Pushmon notification, that we are alive
