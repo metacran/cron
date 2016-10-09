bc0893598c0078625e79db53bdda234cc912623d
Gábor Csárdi <csardi.gabor@gmail.com>
2016-10-09 14:49:38 +0100

    crandb: no need to deal with the pid file
    Jenkins can schedule a single build at a time

--- minutely/update-crandb.r
+++ minutely/update-crandb.r
@@ -4,27 +4,6 @@
 ## on.exit.
 
 do <- function() {
-  out_dir <- Sys.getenv("OPENSHIFT_REPO_DIR")
-  pid_file <- file.path(out_dir, "update-crandb.pid")
-
-  ## If the pid file exists, then we only try in every five
-  ## minutes. In case the previous process got stuck
-  minute <- as.numeric(format(Sys.time(), "%M"))
-  if (file.exists(pid_file) && minute %% 5) { return() }
-
-  ## OK, here we go, write pid file
-  pid <- Sys.getpid()
-  cat(pid, "\n", file = pid_file)
-
-  ## Remove pid file, but only if it was written by us.
-  ## Yes, this is a race condition, but it not critical if
-  ## we remove someone else's pid
-  clean_pid <- function() {
-    if (scan(pid_file, quiet = TRUE)[1] == pid) {
-      try(unlink(pid_file), silent = TRUE)
-    }
-  }
-  on.exit(clean_pid(), add = TRUE)
 
   ## Load packages
   library(methods)
@@ -38,7 +17,7 @@ do <- function() {
   options(repos = structure(c(CRAN = "http://cran.r-project.org")))
 
   ## Do the update, and log it
-  data_dir <- Sys.getenv("OPENSHIFT_DATA_DIR")
+  data_dir <- "."
   log_file <- file.path(data_dir, "update-crandb.log")
   cat(format(Sys.time()), " updating... ", file = log_file, append = TRUE)
   events <- crandb:::crandb_update()
